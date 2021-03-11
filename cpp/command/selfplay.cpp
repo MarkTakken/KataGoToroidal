bf384641b898241afd57b17cc1af5f38c0359e85
Mark Takken <marktakken@Marks-MacBook-Air.local>
2021-03-11 15:25:55 -0500

    debugging

--- cpp/command/selfplay.cpp
+++ cpp/command/selfplay.cpp
@@ -96,6 +96,7 @@ int MainCmds::selfplay(int argc, const char* const* argv) {
 
   //Width and height of the board to use when writing data, typically 19
   const int dataBoardLen = cfg.getInt("dataBoardLen",9,37);
+  cout << "************** dataBoardLen = " + to_string(dataBoardLen) + "********";
   assert(dataBoardLen == 13);
   const int inputsVersion =
     cfg.contains("inputsVersion") ?
