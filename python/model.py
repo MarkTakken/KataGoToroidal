3690a09fe223d07637b7f962b1ff856cc73de138
Mark Takken <marktakken@Marks-MacBook-Air.local>
2021-03-11 17:00:53 -0500

    debugging

--- python/model.py
+++ python/model.py
@@ -610,6 +610,7 @@ class Model:
   def pad_toroidal(tensor,yx_pad):
     (y_pad,x_pad) = yx_pad
     (nsize,csize,ysize,xsize) = tensor.shape
+    print(tensor.shape)
     tensor = tf.concat([tf.gather(tensor,range(ysize-y_pad,ysize),axis=2),tensor,tf.gather(tensor,range(y_pad),axis=2)],axis=2)
     tensor = tf.concat([tf.gather(tensor,range(xsize-x_pad,xsize),axis=3),tensor,tf.gather(tensor,range(x_pad),axis=3)],axis=3)
     return tensor
